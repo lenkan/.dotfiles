390a671d667e45ef628c48da311418090a44dfb1
lenkan <dlenksjo@gmail.com>
2022-10-31 08:33:54 +0100

    update gh

--- install-gh.sh
+++ install-gh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd tmp
-version=2.14.7
+version=2.18.1
 filename=gh_"$version"_linux_386.deb
 wget "https://github.com/cli/cli/releases/download/v$version/$filename"
 sudo dpkg -i $filename
