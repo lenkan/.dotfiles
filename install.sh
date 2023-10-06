e541ddd36bbebb75647486a0780c76138cc4a29e
lenkan <dlenksjo@gmail.com>
2023-10-06 09:16:49 +0200

    add azure install script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    echo "Not on WSL!"
+else
+    echo "On WSL!"
+fi
+
 sudo rm -rf /var/cache/snapd/
 sudo apt autoremove --purge snapd gnome-software-plugin-snap
 rm -fr ~/snap
@@ -15,18 +21,7 @@ sudo apt-get install \
 	lsb-release \
 	apt-transport-https
 
-# Google https://www.ubuntuupdates.org/ppa/google_chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
-
-# Microsoft
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >./tmp/microsoft.gpg
-sudo install -o root -g root -m 644 ./tmp/microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
 sudo apt-get update
-sudo apt-get install google-chrome-stable
-sudo apt-get install code
 sudo apt-get install tmux
 sudo apt-get install xclip
 sudo apt-get install jq
