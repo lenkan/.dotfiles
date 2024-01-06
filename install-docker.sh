b0a66f5c71016ea757cdd519b72553af573f1548
lenkan <dlenksjo@gmail.com>
2024-01-06 13:07:37 +0100

    update docker install

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-filename=docker-desktop-4.17.0-amd64.deb
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor >./tmp/docker.gpg
+sudo install -o root -g root -m 644 ./tmp/docker.gpg /etc/apt/trusted.gpg.d/
+
+arch=$(dpkg --print-architecture)
+filename=docker-desktop-4.26.1-amd64.deb
+release=$(. /etc/os-release && echo "$VERSION_CODENAME")
+
+echo "deb [arch="$arch" signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu $release stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+
 rm -r "tmp/$filename"
 wget -P tmp/ "https://desktop.docker.com/linux/main/amd64/$filename"
 sudo apt install "./tmp/$filename"
