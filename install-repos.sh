dce3e6e3846da8cc8b2f42bc3bbf38141faf240e
lenkan <5889538+lenkan@users.noreply.github.com>
2025-11-14 09:14:55 +0100

    add 1password

--- install-repos.sh
+++ install-repos.sh
@@ -29,10 +29,12 @@ install_gpg() {
 install_gpg "https://cli.github.com/packages/githubcli-archive-keyring.gpg" "githubcli.gpg"
 install_gpg "https://packages.microsoft.com/keys/microsoft.asc" "packages.microsoft.gpg"
 install_gpg "https://download.docker.com/linux/ubuntu/gpg" "docker.gpg"
+install_gpg "https://downloads.1password.com/linux/keys/1password.asc" "1password.gpg"
 
 echo "deb [arch=$arch signed-by=$gpgdir/githubcli.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list
 echo "deb [arch=$arch signed-by=$gpgdir/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 echo "deb [arch=$arch signed-by=$gpgdir/packages.microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $dist main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 echo "deb [arch=$arch signed-by=$gpgdir/docker.gpg] https://download.docker.com/linux/ubuntu $dist stable" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb [arch=$arch signed-by=$gpgdir/1password.gpg] https://downloads.1password.com/linux/debian/$arch stable main" | sudo tee /etc/apt/sources.list.d/1password.list
 
 sudo apt update
