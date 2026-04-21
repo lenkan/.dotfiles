29e65d326bd56f190b2b8c09ee798675b02d2902
lenkan <5889538+lenkan@users.noreply.github.com>
2026-04-21 22:41:18 +0200

    fix sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,7 +2,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 for filename in "$DIR"/.*; do
-	if [ ! -d "$filename" ]; then
+	if [ -f "$filename" ]; then
 		ln -svf "$filename" "$HOME/$(basename "$filename")"
 	fi
 done
@@ -10,7 +10,7 @@ done
 # Copy .local/bin scripts
 mkdir -p "$HOME/.local/bin"
 for filename in "$DIR"/.local/bin/*; do
-	if [ ! -d "$filename" ]; then
+	if [ -f "$filename" ]; then
 		ln -svf "$filename" "$HOME/.local/bin/$(basename "$filename")"
 	fi
 done
