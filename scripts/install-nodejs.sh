9fc3c17aba68a7bd38ae4e1e7f3eea197a5b7f53
lenkan <5889538+lenkan@users.noreply.github.com>
2026-04-21 22:14:20 +0200

    cleanup nvm

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 sudo apt update && sudo apt install -y curl unzip
+rm -rf "$HOME/.nvm"
+
 command -v fnm &>/dev/null || curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.local/bin" --skip-shell
 
 "$HOME/.local/bin/fnm" install --lts
